33e4c8479ec273dab4901ef423c125d07272f77c
Fenyves96 <fenyves96@hotmail.com>
2018-03-03 00:57:46 +0100

    Body menu almost done.

--- app/src/main/java/com/example/fenyv/fittdroiddrawer/DBHandler.java
+++ app/src/main/java/com/example/fenyv/fittdroiddrawer/DBHandler.java
@@ -1,37 +1,87 @@
 package com.example.fenyv.fittdroiddrawer;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by fenyv on 2018. 03. 01..
  */
 
 public class DBHandler extends SQLiteOpenHelper {
-    public final String DATABASE_NAME="FittDroid.db";
-    public final String TABLE_NAME="Statistics";
-    public final String COL_DATE="Date";
-    public final String COL_HEIGHT="Neck";
-    public final String COL_WEIGHT="Weight";
-    public final String COL_HIP="Hip";
-    public final String COL_NECK="Neck";
+    public static final String DATABASE_NAME="FittDroid.db";
+    public static final String TABLE_NAME="Statistics";
+    public static final String COL_DATE="Date";
+    public static final String COL_HEIGHT="Height";
+    public static final String COL_WEIGHT="Weight";
+    public static final String COL_WAIST="Waist";
+    public static final String COL_HIP="Hip";
+    public static final String COL_NECK="Neck";
 
 
 
+    Context c;
 
 
-    public DBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        c=context;
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+ TABLE_NAME+"("+
+                COL_DATE+" DATE  PRIMARY KEY,"+
+                COL_HEIGHT+" FLOAT,"+
+                COL_WEIGHT+" FLOAT,"+
+                COL_WAIST+" FLOAT ," +
+                COL_HIP+" FLOAT,"+
+                COL_NECK+" FLOAT )");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("CREATE TABLE  "+ TABLE_NAME+"("+
+                COL_DATE+" DATE  PRIMARY KEY,"+
+                COL_HEIGHT+" FLOAT,"+
+                COL_WEIGHT+" FLOAT,"+
+                COL_WAIST+" FLOAT ," +
+                COL_HIP+" FLOAT,"+
+                COL_NECK+" FLOAT )");
 
+    }
+
+    public boolean insertData(float height, float weight,float hip,float waist,float neck){
+
+
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        db.delete(TABLE_NAME,"Date='"+date+"'",null);
+        contentValues.put(COL_DATE,date);
+        contentValues.put(COL_HEIGHT,height);
+        contentValues.put(COL_WEIGHT,weight);
+        contentValues.put(COL_WAIST,weight);
+        contentValues.put(COL_HIP,hip);
+        contentValues.put(COL_NECK,neck);
+        long result=db.insert(TABLE_NAME,null,contentValues);
+        if(result==-1){
+            return  false;
+        }
+
+        return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor res=db.rawQuery("SELECT * FROM "+TABLE_NAME+" WHERE Date='"+"2018-03-03'",null);
+        return  res;
     }
 }
--- app/src/main/java/com/example/fenyv/fittdroiddrawer/body_menu.java
+++ app/src/main/java/com/example/fenyv/fittdroiddrawer/body_menu.java
@@ -1,5 +1,6 @@
 package com.example.fenyv.fittdroiddrawer;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -18,11 +20,17 @@ public class body_menu extends Fragment {
 
     TextView calcTv;
     Button calcbtn;
+    EditText etWeight;
+    EditText etHeight;
+    EditText etWaist;
+    EditText etHip;
+    EditText etNeck;
+    //DB handlerre itt szükség lesz
+    DBHandler dbHandler;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
+
         return inflater.inflate(R.layout.fragment_body, container, false);
     }
 
@@ -31,15 +39,52 @@ public class body_menu extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
+        dbHandler=new DBHandler(getActivity().getBaseContext()); //initialize dbHandler
         getActivity().setTitle("Body");
         calcTv=view.findViewById(R.id.tvcalc);
         calcbtn=view.findViewById(R.id.calcbtn);
+        etHeight=getActivity().findViewById(R.id.etheight);
+        etWeight=getActivity().findViewById(R.id.etweight);
+        etWaist=getActivity().findViewById(R.id.etwaist);
+        etHip=getActivity().findViewById(R.id.ethip);
+        etNeck=getActivity().findViewById(R.id.etneck);
         calcbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcTv.setText("Body fat: 18%");
+                float height=Float.valueOf(etHeight.getText().toString());
+                float weight=Float.valueOf(etWeight.getText().toString());
+                float waist=Float.valueOf(etWaist.getText().toString());
+                float hip=Float.valueOf(etHip.getText().toString());
+                float neck=Float.valueOf(etNeck.getText().toString());
+                calcTv.setText(Float.toString(height)+" "+Float.toString(waist));
+                boolean inserted=dbHandler.insertData(height,weight,waist,hip,neck);
+                if(inserted){
+                    calcTv.setText("yeah");
+                }else {calcTv.setText("nooo");}
             }
         });
+        loadEditTexts();
+    }
+
+    void loadEditTexts(){
+        Cursor cursor= dbHandler.getAllData();
+        if(cursor.getCount()==0){
+            return;
+        }
+        if(cursor.moveToFirst()) {
+            float height = cursor.getFloat(cursor.getColumnIndex(DBHandler.COL_HEIGHT));
+            float weight = cursor.getFloat(cursor.getColumnIndex("Weight"));
+            float waist = cursor.getFloat(cursor.getColumnIndex("Waist"));
+            float hip = cursor.getFloat(cursor.getColumnIndex("Hip"));
+            float neck = cursor.getFloat(cursor.getColumnIndex("Neck"));
+
+
+            etHeight.setText(Float.toString(height));
+            etWeight.setText(Float.toString(weight));
+            etWaist.setText(Float.toString(waist));
+            etHip.setText(Float.toString(hip));
+            etNeck.setText(Float.toString(neck));
+        }
     }
 
 
